5c1d6462f6ff2ab7abeb8368b7f67a7975fa0c8c
MysticalDevil <jinpeng7981@outlook.com>
2024-01-20 17:46:56 +0800

    Add some tools integration

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -5,9 +5,15 @@ end
 function starship_transient_prompt_func
     starship module character
 end
-# Starship -- Cross platform shell prompt
+# starship -- Cross platform shell prompt
 starship init fish | source
 enable_transience
+
+# zoxide -- A smarter cd command. Supports all major shells. 
+zoxide init fish --cmd cd | source
+
+# direnv -- unclutter your .profile 
+direnv hook fish | source
 
 # Config fzf.fish
 set fzf_preview_dir_cmd eza --all --long --color=always
